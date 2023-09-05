f3bb98a63085f14e423c5ec0ed2e6215dad23aee
Bhupender98 <bhupander17440@gmail.com>
2023-09-05 14:51:25 +0530

    updated

--- config/key.js
+++ config/key.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MongoURI: "mongodb://admin:admin987@cluster0-shard-00-00.r3fs6.mongodb.net:27017,cluster0-shard-00-01.r3fs6.mongodb.net:27017,cluster0-shard-00-02.r3fs6.mongodb.net:27017/auth?authSource=admin&replicaSet=atlas-638q0p-shard-0&readPreference=primary&ssl=true"
+    // MongoURI: "mongodb://admin:admin987@cluster0-shard-00-00.r3fs6.mongodb.net:27017,cluster0-shard-00-01.r3fs6.mongodb.net:27017,cluster0-shard-00-02.r3fs6.mongodb.net:27017/auth?authSource=admin&replicaSet=atlas-638q0p-shard-0&readPreference=primary&ssl=true"
+    MongoURI: `mongodb+srv://bhupander17440:kumar12345@cluster0.biz9oc0.mongodb.net/?retryWrites=true&w=majority`
 }
